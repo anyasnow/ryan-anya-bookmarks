05c1082634e4b0d12805f94f47e2a3782f593511
Anya Snow <emailasnow@gmail.com>
2019-05-11 13:34:14 -0700

    add in tests with databases

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
-const bookmarks = require('./store.js');
-const bookmarkRouter = require('../bookmarks/bookmarks.js')
+const BookmarksService = require('./bookmarks-service')
+// const bookmarks = require('./store.js');
+// const bookmarkRouter = require('../bookmarks/bookmarks.js')
 const logger = require('../bookmarks/logger.js')
 
 const app = express()
@@ -26,26 +27,42 @@ app.use(function validateBearerToken(req, res, next) {
 })
 
 const morganOption = (NODE_ENV === 'production')
-    ? 'tiny'
-    : 'common';
+  ? 'tiny'
+  : 'common';
 
 app.use(morgan(morganOption))
 app.use(cors())
 app.use(helmet())
 app.use(express.json());
-app.use(bookmarkRouter)
+// app.use(bookmarkRouter)
 
-app.get('/bookmarks', (req, res) => {
-  res
-    .json(bookmarks);
+app.get('/bookmarks', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  BookmarksService.getAllBookmarks(knexInstance)
+    .then(bookmarks => {
+      res.json(bookmarks)
+    })
+    .catch(next)
 });
 
 
+app.get('/bookmarks/:bookmark_id', (req, res, next) => {
+  const knexInstance = req.app.get('db')
+  BookmarksService.getById(knexInstance, req.params.bookmark_id)
+    .then(bookmark => {
+      if (!bookmark) {
+        return res.status(404).json({
+          error: { message: `Bookmark doesn't exist` }
+        })
+      }
+      res.json(bookmark)
+    })
+    .catch(next)
+})
 
 
 
-  
-  
+
 
 
 
@@ -53,15 +70,15 @@ app.get('/bookmarks', (req, res) => {
 
 
 app.use(function errorHandler(error, req, res, next) {
-      let response
-    if (NODE_ENV === 'production') {
-                response = { error: { message: 'server error' } }
-              } else {
-            console.error(error)
-            response = { message: error.message, error }
-          }
-      res.status(500).json(response)
-        })
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
 
 
 module.exports = app
